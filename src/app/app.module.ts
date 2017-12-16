379592edc96f0f6ae6d60fb0364289c9dc0b791f
Qingyuan <qingyuan1017@gmail.com>
2017-12-16 16:03:31 -0500

    Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import 'hammerjs';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-
+import { DishService} from './services/dish.service'
 
 @NgModule({
   declarations: [
@@ -32,7 +32,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatSlideToggleModule, MatToolbarModule, MatListModule, MatGridListModule,
     MatCardModule, MatIconModule, MatProgressSpinnerModule, MatDialogModule,FlexLayoutModule
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/dish.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Dish } from '../share/dish';
+import { DISHES} from '../share/dishes'
+
+@Injectable()
+export class DishService {
+
+  constructor() { }
+
+  getDishes(): Dish[]{
+    return DISHES;
+  }
+
+}
